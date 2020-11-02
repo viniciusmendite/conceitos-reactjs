175831f42ccf879fdb723841a01565301794c6b8
Vinicius Mendite <vinicius.miff@gmail.com>
2020-11-02 11:02:48 -0300

    aplicando conceitos de propriedade

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header/>
-      <Header/>
+      <Header title="Homepage">
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </Header>
+      <Header title="projects">
+        <ul>
+          <li>Item 4</li>
+          <li>Item 5</li>
+        </ul>
+      </Header>
     </>
   );
 }
